430588e748dd705a7537982161e14bff86b071b3
Mateusz <matpia98@gmail.com>
2024-07-26 11:08:02 +0200

    implemented creating data for simulation with adding do database

--- src/main/java/com/example/epidemicsimulator/domain/simulationcreator/SimulationAdder.java
+++ src/main/java/com/example/epidemicsimulator/domain/simulationcreator/SimulationAdder.java
@@ -5,8 +5,10 @@ import com.example.epidemicsimulator.domain.simulationcreator.dto.SimulationRequ
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
-
+import java.util.Queue;
 
 @Service
 @AllArgsConstructor
@@ -19,11 +21,76 @@ class SimulationAdder {
         Simulation simulationToSave = SimulationMapper.mapFromSimulationRequestDtoToSimulation(requestDto);
         Simulation savedSimulation = simulationRepository.save(simulationToSave);
 
+        List<DailyData> dailyDataList = runSimulation(savedSimulation);
+        dailyDataRepository.saveAll(dailyDataList);
+
         return SimulationMapper.mapFromSimulationToSimulationDto(savedSimulation);
     }
 
     private List<DailyData> runSimulation(Simulation simulation) {
-        return null;
-    }
+        List<DailyData> dailyDataList = new ArrayList<>();
 
+        int susceptible = simulation.getPopulationSize() - simulation.getInitialInfected();
+        int infected = simulation.getInitialInfected();
+        int deceased = 0;
+        int recovered = 0;
+
+        int Tm = simulation.getDeathDuration();
+        int Ti = simulation.getInfectionDuration();
+        double R = simulation.getInfectionRate();
+        int days = simulation.getSimulationDuration();
+
+        Queue<Integer> infectionQueue = new LinkedList<>();
+        Queue<Integer> deathQueue = new LinkedList<>();
+
+        for (int day = 1; day <= days; day++) {
+            if (day >= Tm) {
+                int newlyDeceased = deathQueue.poll();
+                newlyDeceased = Math.min(newlyDeceased, infected);
+                infected -= newlyDeceased;
+                deceased += newlyDeceased;
+            }
+
+            if (day >= Ti) {
+                int newlyRecovered;
+                if (day == Ti) {
+                    newlyRecovered = infectionQueue.poll() + simulation.getInitialInfected();
+                } else {
+                    newlyRecovered = infectionQueue.poll();
+                }
+                    newlyRecovered = Math.min(newlyRecovered, infected);
+                    infected -= newlyRecovered;
+                    recovered += newlyRecovered;
+            }
+
+            int newlyInfected = (int) (infected * R);
+            if (newlyInfected > susceptible) {
+                newlyInfected = susceptible;
+            }
+            susceptible -= newlyInfected;
+            infected += newlyInfected;
+
+            infectionQueue.add(newlyInfected);
+            deathQueue.add((int) (newlyInfected * simulation.getMortalityRate()));
+
+            infected = Math.max(infected, 0);
+            susceptible = Math.max(susceptible, 0);
+            deceased = Math.max(deceased, 0);
+            recovered = Math.max(recovered, 0);
+
+            DailyData dailyData = new DailyData(
+                    null,
+                    day,
+                    infected,
+                    susceptible,
+                    deceased,
+                    recovered,
+                    simulation
+            );
+
+            dailyDataList.add(dailyData);
+        }
+
+        return dailyDataList;
+    }
 }
